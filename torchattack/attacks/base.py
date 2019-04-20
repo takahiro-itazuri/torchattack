5fe0832a61e48ba9236f7917eaa08f3f497511a1
Takahiro Itazuri <takahiro.lab.1226@gmail.com>
2019-04-21 07:44:48 +0900

    add documents

--- torchattack/attacks/base.py
+++ torchattack/attacks/base.py
@@ -4,6 +4,12 @@ from torch import nn
 from abc import *
 
 class BaseAttack(ABC):
+    """Base class for adversarial attacks.
+
+    Args:
+        model (torch.nn.Module): Model to be attacked
+        criterion (torch.nn.Module): Criterion to calculate loss.
+    """
     def init(self, model, criterion=nn.CrossEntropyLoss()):
         self.model = model
         self.criterion = criterion
