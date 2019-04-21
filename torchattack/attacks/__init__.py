300d92d457d649bcca19284eee82d6cf5b4280f3
Takahiro Itazuri <takahiro.lab.1226@gmail.com>
2019-04-21 11:50:22 +0900

    add FGSMAttack into __init__.py and remove __init__ method from FGSMAttack

--- torchattack/attacks/__init__.py
+++ torchattack/attacks/__init__.py
@@ -1,1 +1,2 @@
 from .base import BaseAttack
+from .fgsm import FGSMAttack
--- torchattack/attacks/fgsm.py
+++ torchattack/attacks/fgsm.py
@@ -4,10 +4,6 @@ from torch import nn
 from .base import BaseAttack
 
 class FGSMAttack(BaseAttack):
-    def init(self, model, criterion=nn.CrossEntropyLoss()):
-        self.model = model
-        self.criterion = criterion
-
     def attack(self, x, t=None):
         return NotImplementedError
 
