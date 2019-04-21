e348a588f4d3554533670e4899b275f162fcd12c
Takahiro Itazuri <takahiro.lab.1226@gmail.com>
2019-04-21 12:44:14 +0900

    Merge branch 'develop' into features/fgsm

--- torchattack/attacks/fgsm.py
+++ torchattack/attacks/fgsm.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+from torch.autograd import grad
 
 from .base import BaseAttack
 
@@ -19,9 +20,30 @@ class FGSMAttack(BaseAttack):
     """
     def __init__(self, model, criterion=nn.CrossEntropyLoss(), mean=None, std=None, eps=0.3, p=-1):
         BaseAttack.__init__(self, model, criterion, mean, std)
-        self.eps = eps
+        self.eps = self._scale(eps)[None, :, None, None]
         self.p = p
 
-    def attack(self, x, t=None):
-        return NotImplementedError
+    def attack(self, x, t):
+        """Given pais of (``x``, ``t``), returns adversarial examples.
+
+        Args:
+            x (torch.Tensor): Input tensor.
+            t (torch.LongTensor): Label tensor.
+        """
+        assert self._is_tensor_image(x), "The input tensor should be tensor image (NCHW)."
+        device = x.device
+        eps = self.eps.to(device)
+        delta = torch.zeros_like(x, requires_grad=True).to(device)
+
+        perturbed_x = x + delta
+        y = self.model(perturbed_x if perturbed_x.shape[1] == 3 else perturbed_x.repeat(1, 3, 1, 1))
+        loss = criterion(y, t)
+        grad_delta = grad(loss, delta)[0].detach()
+
+        if p == -1:
+            delta.data = eps * grad_delta.sign()
+        else:
+            raise NotImplementedError
+        
+        return self._clamp(x + delta).detach()
 
